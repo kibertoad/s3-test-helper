d036ca3ed22fc3ba871e8353f19db6fc6677cdb6
Igor Savin <iselwin@gmail.com>
2022-07-19 18:57:05 +0300

    Export options type

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
 export { S3TestHelper } from './lib/s3TestHelper'
-export type { Logger, FileDefinition } from './lib/s3TestHelper'
+export type { Logger, FileDefinition, S3TestHelperOptions } from './lib/s3TestHelper'
